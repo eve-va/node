ee0d516e9e320d0ecb70a3bb2dbd708fbe5bbfce
eve-va <yeva.vakhtina1@gmail.com>
2021-07-25 19:28:27 +0300

    Callbacks and Error Handling

--- node-examples/rectangle.js
+++ node-examples/rectangle.js
@@ -1,7 +1,15 @@
-exports.perimeter =  (x, y) => (2*(x+y));
-exports.area = (x, y) => (x*y);
-
-/*
-export function perimeter(x, y) { return (2*(x+y)); }
-export function area(x, y) { return (x*y); }
-*/
+module.exports = (x,y,callback) => {
+  if (x <= 0 || y <= 0)
+      setTimeout(() => 
+          callback(new Error("Rectangle dimensions should be greater than zero: l = "
+              + x + ", and b = " + y), 
+          null),
+          2000);
+  else
+      setTimeout(() => 
+          callback(null, {
+              perimeter: () => (2*(x+y)),
+              area:() => (x*y)
+          }), 
+          2000);
+}
